ac82e960a02ffa087ebde7a6cab4046d9fe42c55
rmatta5 <rmatta5@illinois.edu>
2018-02-06 16:52:42 -0600

    final project commit

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,30 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
 public class WebScraper {
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        String text = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        String[] words = text.split("[ ,.?]+");
+        int count = 0;
+        String lookFor = "prince";
+        for (String word : words) {
+            if (word.toLowerCase().equals(lookFor)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
 }
+
